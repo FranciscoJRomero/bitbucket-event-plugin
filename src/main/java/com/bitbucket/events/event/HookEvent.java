06dd7703be63a45d33256e509c89f082a39ebc28
francisco <franciscoromero02@gmail.com>
2018-12-29 20:13:44 -0500

    attempted to seperate hooks

--- src/main/java/com/bitbucket/events/event/HookEvent.java
+++ src/main/java/com/bitbucket/events/event/HookEvent.java
@@ -6,42 +6,56 @@ import com.atlassian.bitbucket.event.hook.RepositoryHookEvent;
 import com.atlassian.bitbucket.event.hook.RepositoryHookSettingsChangedEvent;
 import com.atlassian.bitbucket.hook.repository.RepositoryHookService;
 import com.atlassian.event.api.EventListener;
+import com.atlassian.event.api.EventListenerRegistrar;
+import com.atlassian.event.api.EventPublisher;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-@Named("HookEvent")
+@Named("HookEventSomeUniqueName")
 public class HookEvent {
     private static Logger log = LoggerFactory.getLogger(HookEvent.class);
-
+    @ComponentImport
+    EventListenerRegistrar eventListener;
     @ComponentImport
     RepositoryHookService hookService;
+    @ComponentImport
+    EventPublisher eventPublisher;
 
 
     @Inject
-    private HookEvent(RepositoryHookService hookService) {
+    private HookEvent(RepositoryHookService hookService, EventListenerRegistrar eventListener,
+                      EventPublisher eventPublisher) {
         log.error("Registered Hook Event");
         System.out.println("Something constructor");
         this.hookService = hookService;
+        this.eventListener = eventListener;
+        this.eventPublisher = eventPublisher;
+        this.eventListener.register(this);
+        this.eventPublisher.publish(this);
     }
 
-    @EventListener
+    public void finalize() {
+        eventListener.unregister(this);
+    }
+
+    @EventListener(scope = "Work 1!")
     public void onHookModified(RepositoryHookSettingsChangedEvent event) {
         System.out.println("Something");
     }
 
-    @EventListener
+    @EventListener(scope = "Work 2!")
     public void onHookModified(RepositoryHookEvent event) {
         log .error("\nEvent Scope = " + event.getScope());
-        log .error("\nEvent Source = " + event.getSource());
+        log .error("\nEvent Source =  " + event.getSource());
         log.error("\nEvent Date = " + event.getDate().toString());
         log .error("\nEvent User = " + event.getUser().toString());
         System.out.println("Something hook event");
     }
 
-    @EventListener
+    @EventListener(scope = "Work 3!")
     public void onHookDisabled(RepositoryHookDisabledEvent event) {
         log .error("\nEvent Scope = " + event.getScope());
         log .error("\nEvent Source = " + event.getSource());
@@ -51,7 +65,7 @@ public class HookEvent {
         hookService.enable(event.getRepository(),event.getRepositoryHookKey());
     }
 
-    @EventListener
+    @EventListener(scope = "Work!")
     public void onRepositoryHookEnabledEvent(RepositoryHookEnabledEvent enabledEvent) {
         log.info("Repo enabled");
         log.error("Error stuff");
@@ -59,5 +73,4 @@ public class HookEvent {
         log.debug("Event to string: " + enabledEvent.toString());
         log.warn("Warning from enabling\n\n");
     }
-
 }
--- src/main/java/com/bitbucket/events/hook/PreReceiveHookValidator.java
+++ src/main/java/com/bitbucket/events/hook/PreReceiveHookValidator.java
@@ -2,27 +2,33 @@ package com.bitbucket.events.hook;
 
 import com.atlassian.bitbucket.hook.repository.*;
 import com.atlassian.bitbucket.repository.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
 
-public class PreReceiveHookValidator implements PreRepositoryHook
+public class PreReceiveHookValidator implements PreRepositoryHook<RepositoryHookRequest>
 {
+    private static Logger log = LoggerFactory.getLogger(PreReceiveHookValidator.class);
+
     @Nonnull
     @Override
-    public RepositoryHookResult preUpdate(@Nonnull PreRepositoryHookContext preRepositoryHookContext, @Nonnull RepositoryHookRequest repositoryHookRequest) {
-
-        for (RefChange refChange : repositoryHookRequest.getRefChanges())
-        {
-            if (refChange.getType() == RefChangeType.DELETE)
-            {
-                repositoryHookRequest.getScmHookDetails().get().err().println("The ref '" + refChange.getRef().getId() + "' cannot be deleted.");
-                return RepositoryHookResult.rejected("Rejected Delete for no reason","something happened");
-            } else if (refChange.getType() == RefChangeType.ADD) {
-                return RepositoryHookResult.rejected("Rejected Add for no reason","something happened");
-            } else {
-                return RepositoryHookResult.rejected("Rejected Update for no reason","something happened");
-            }
-        }
+    public RepositoryHookResult preUpdate(@Nonnull PreRepositoryHookContext preRepositoryHookContext,
+                                          @Nonnull RepositoryHookRequest repositoryHookRequest) {
+//
+//        for (RefChange refChange : repositoryHookRequest.getRefChanges()) {
+//            log.error("Ref Change: " + refChange.toString());
+//            if (refChange.getType() == RefChangeType.DELETE) {
+//                return RepositoryHookResult.rejected("Rejected Delete for no reason",
+//                        "something happened");
+//            } else if (refChange.getType() == RefChangeType.ADD) {
+//                return RepositoryHookResult.rejected("Rejected Add for no reason",
+//                        "something happened");
+//            } else {
+//                return RepositoryHookResult.rejected("Rejected Update for no reason",
+//                        "something happened");
+//            }
+//        }
         return RepositoryHookResult.accepted();
     }
 }
